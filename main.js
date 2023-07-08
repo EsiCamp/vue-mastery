149a54db7e0349b2848796f3892e1089a4289e88
Ehsan Tashakkori <ehsan.catalan@gmail.com>
2023-07-08 10:38:30 +0330

    done!

---
+++ main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    product: "Socks",
+    description: "A pair of warm, fuzzy socks",
+    imageSrc: "./assets/socks-green.png",
+    altText: "A pair of Socks",
+    link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
+    inStock: true,
+    onSale: true,
+  },
+});
