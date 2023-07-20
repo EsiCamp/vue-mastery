a3463706ba311330aca504366bd01e4b85e1c54e
Ehsan Tashakkori <ehsan.catalan@gmail.com>
2023-07-21 01:16:36 +0330

    do challenges (List Rendering and Event Handling)

--- main.js
+++ main.js
@@ -3,10 +3,36 @@ var app = new Vue({
   data: {
     product: "Socks",
     description: "A pair of warm, fuzzy socks",
-    imageSrc: "./assets/socks-green.png",
+    imageSrc: "./assets/socks-green.jpg",
     altText: "A pair of Socks",
     link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
     inStock: true,
     onSale: true,
+    details: ["80% cotton", "20% polyester", "Gender-neutral"],
+    variants: [
+      {
+        variantId: 2234,
+        variantColor: "green",
+        variantImg: "./assets/socks-green.jpg",
+      },
+      {
+        variantId: 2235,
+        variantColor: "blue",
+        variantImg: "./assets/socks-blue.jpg",
+      },
+    ],
+    sizes: ["Small", "Medium", "Large", "XLarge"],
+    cart: 0,
+  },
+  methods: {
+    addToCart() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      this.cart -= 1;
+    },
+    updateProduct(variantImg) {
+      this.imageSrc = variantImg;
+    },
   },
 });
