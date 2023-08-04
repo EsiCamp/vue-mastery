e52b0b74043ec579b935d5f8866af9869abeb152
Ehsan Tashakkori <ehsan.catalan@gmail.com>
2023-08-04 21:14:04 +0330

    doing challenge (Components)

--- main.js
+++ main.js
@@ -1,30 +1,87 @@
-var app = new Vue({
-  el: "#app",
-  data: {
-    brand: "Vue Mastery",
-    product: "Socks",
-    description: "A pair of warm, fuzzy socks",
-    selectedVariant: 0,
-    altText: "A pair of Socks",
-    link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
-    details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    variants: [
-      {
-        variantId: 2234,
-        variantColor: "green",
-        variantImg: "./assets/socks-green.jpg",
-        variantQuantity: 10,
-      },
-      {
-        variantId: 2235,
-        variantColor: "blue",
-        variantImg: "./assets/socks-blue.jpg",
-        variantQuantity: 0,
-      },
-    ],
-    onSale: true,
-    sizes: ["Small", "Medium", "Large", "XLarge"],
-    cart: 0,
+Vue.component("product-details", {
+  props: {
+    details: {
+      type: Array,
+      required: true,
+    },
+  },
+  template: `
+    <ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>
+  `,
+});
+
+Vue.component("product", {
+  props: {
+    premium: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  template: `
+    <div class="product">
+      <div class="product-image">
+        <a :href="link" :title="altText" class="res">
+          <img :src="imageSrc" :alt="altText" />
+        </a>
+      </div>
+      <div class="product-info">
+        <h1>{{ title }}</h1>
+        <p>{{ description }}</p>
+        <p v-if="inStock">In Stock</p>
+        <p v-else :class="{ outOfStock: !inStock }">Out of Stock</p>
+        <p>Shipping: {{ shipping }}</p>
+        <p>{{ sale }}</p>
+        <product-details :details="details"></product-details>
+        <div
+          v-for="(variant, index) in variants"
+          :key="variant.variantId"
+          class="color-box"
+          :style="{ backgroundColor: variant.variantColor }"
+          @mouseover="updateProduct(index)"
+        ></div>
+        <button
+          @click="addToCart()"
+          :disabled="!inStock"
+          :class="{ disabledButton: !inStock }"
+        >
+          Add to cart
+        </button>
+        <button @click="removeFromCart()">Remove from cart</button>
+        <div class="cart">
+          <p>Cart({{ cart }})</p>
+        </div>
+      </div>
+    </div>
+  `,
+  data() {
+    return {
+      brand: "Vue Mastery",
+      product: "Socks",
+      description: "A pair of warm, fuzzy socks",
+      selectedVariant: 0,
+      altText: "A pair of Socks",
+      link: "https://www.vuemastery.com/courses/intro-to-vue-js/attribute-binding",
+      details: ["80% cotton", "20% polyester", "Gender-neutral"],
+      variants: [
+        {
+          variantId: 2234,
+          variantColor: "green",
+          variantImg: "./assets/socks-green.jpg",
+          variantQuantity: 10,
+        },
+        {
+          variantId: 2235,
+          variantColor: "blue",
+          variantImg: "./assets/socks-blue.jpg",
+          variantQuantity: 0,
+        },
+      ],
+      onSale: true,
+      sizes: ["Small", "Medium", "Large", "XLarge"],
+      cart: 0,
+    };
   },
   methods: {
     addToCart() {
@@ -54,5 +111,18 @@ var app = new Vue({
       }
       return `${this.brand} ${this.product} are not on sale!`;
     },
+    shipping() {
+      if (this.premium) {
+        return "Free";
+      }
+      return 2.99;
+    },
+  },
+});
+
+var app = new Vue({
+  el: "#app",
+  data: {
+    premium: true,
   },
 });
